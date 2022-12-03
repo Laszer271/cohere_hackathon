b850732082e734f4f02d9e157279fbaa5e258234
rbkomar <robkomar98@gmail.com>
2022-12-03 19:05:43 +0100

    init repo + story segmentation

---
+++ image_generator/image_generator.py
@@ -0,0 +1,10 @@
+class ImageGenerator:
+    def __init__(self, story_segment, width, height):
+        self.story_segment = story_segment
+        self.width = width
+        self.height = height
+        self.image = None
+
+    def generate_image_with_cohere(self):
+        # here define image generation with cohere for the segmented part of the story
+        pass
